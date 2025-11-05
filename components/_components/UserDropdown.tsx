0a3cdab73015f2eef8754c9a9d5839a566446b91
Arturo Garcia Raygoza <arte311@gmail.com>
2025-11-05 14:08:52 -0700

    Implementacion de landing page

---
+++ components/_components/UserDropdown.tsx
@@ -0,0 +1,97 @@
+import {
+  ChevronDownIcon,
+  HomeIcon,
+  LayoutDashboardIcon,
+  LogOutIcon,
+} from "lucide-react"
+
+import {
+  Avatar,
+  AvatarFallback,
+  AvatarImage,
+} from "@/components/ui/avatar"
+import { Button } from "@/components/ui/button"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import Link from "next/link"
+import { authClient } from "@/lib/auth-client"
+import { useRouter } from "next/navigation"
+import { toast } from "sonner"
+
+interface iAppProps {
+  name: string;
+  email: string;
+  image: string;
+}
+
+export default function UserDropdown({ email, name, image }: iAppProps) {
+  const router = useRouter()
+  const signOut = async () => {
+    await authClient.signOut({
+      fetchOptions: {
+        onSuccess: () => {
+          router.push("/");
+          toast.success("Inicio de sesión exitoso")
+        },
+        onError: () => {
+          toast.error("Error al cerrar sesion")
+        }
+      },
+    });
+  }
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="ghost" className="h-auto p-0 hover:bg-transparent">
+          <Avatar>
+            <AvatarImage src={image} alt="Profile image" />
+            <AvatarFallback>{name[0].toUpperCase()}</AvatarFallback>
+          </Avatar>
+          <ChevronDownIcon
+            size={16}
+            className="opacity-60"
+            aria-hidden="true"
+          />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="max-w-64">
+        <DropdownMenuLabel className="flex min-w-0 flex-col">
+          <span className="truncate text-sm font-medium text-foreground">
+            {name}
+          </span>
+          <span className="truncate text-xs font-normal text-muted-foreground">
+            {email}
+          </span>
+        </DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuGroup>
+          <DropdownMenuItem asChild>
+            <Link href={"/"}>
+              <HomeIcon size={16} className="opacity-60" aria-hidden="true" />
+              <span>Inicio</span>
+            </Link>
+          </DropdownMenuItem>
+          <DropdownMenuItem asChild>
+            <Link href={"/admin"}>
+              <LayoutDashboardIcon size={16} className="opacity-60" aria-hidden="true" />
+              <span>Admin</span>
+            </Link>
+          </DropdownMenuItem>
+        </DropdownMenuGroup>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem onClick={signOut}>
+          <LogOutIcon size={16} className="opacity-60" aria-hidden="true" />
+          <span>Cerrar sesion</span>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
+}
